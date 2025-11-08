50e7d224888658eda02b1dc71b2f285e97d2fc5e
Gal Shaked <gal.shaked55@gmail.com>
2025-11-08 15:14:24 +0200

    Design and structre

--- boolean_retrieval.py
+++ boolean_retrieval.py
@@ -1,3 +1,19 @@
 # This module will implement the boolean retrieval logic.
 class BooleanRetrieval:
+    """
+    function evaluate(query):
+        https://mathworld.wolfram.com/ReversePolishNotation.html
+
+
+    function and_query(term_1_list, term_2_list):
+        See the algorithm in the lecture
+
+    function or_query(term_1_list, term_2_list):
+        merge the lists without duplicates
+
+    function and_not_query(term_1_list, term_2_list):
+        merge the lists
+
+
+    """
     pass
--- inverted_index.py
+++ inverted_index.py
@@ -1,3 +1,27 @@
 # This module will implement the InvertedIndex design and population.
 class InvertedIndex:
+    """
+    Idea:
+    init dict of {term: list_of docs}
+    For each document:
+        for each TEXT tag:
+            for each word:
+                if word not in dict:
+                    create new entry in the dict with the word as a key.
+                    init a new list of docs for this new term and append the current document (incrementalDocId, original_name)
+
+    """
+    pass
+
+class Term:
+    """
+    english word + its frequency
+    * term's frequency should be maintained while building the index with counter.
+    """
+    pass
+
+class Document:
+    """
+    Incremental DocId + original doc identifier.
+    """
     pass
